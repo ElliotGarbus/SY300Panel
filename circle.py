f70d0bb5799deadcfc8681ab618be257a516dd47
sabrah12 <32890571+sabrah12@users.noreply.github.com>
2018-10-20 17:08:56 -0700

    Add files via upload

--- circle.py
+++ circle.py
@@ -13,13 +13,11 @@ from kivy.clock import time
 kv = '''
 #-------------------------------
 # Knob class
-#  Properties are: knob_title, knob_vals, knob_ndx 
+#  Properties are: knob_title, knob_vals, knob_ndx , knob_turn_delay
 #
 # Notes:
-# 1) might also want to add knob_turn_delay
-#    see note about "delta" in python code
-#
-# 2) text size needs to scale with widget size
+# 1) text size needs to scale with widget size
+# 2) pick between x and y (delta abs thinggy)
 #-------------------------------
 <MyFirstKnob>
     orientation: 'vertical'
@@ -76,42 +74,55 @@ BoxLayout:
         MyFirstKnob:
             id: primo
             knob_title: "Pulse Width"
-            knob_vals: [str(x) for x in range(0,201)]
+            knob_vals:  [ str(x) for x in range(101)]
+            knob_turn_delay: .008
         MyFirstKnob:
-            knob_vals: [str(x) for x in range(-24,25)]
+            knob_vals: [ 'L2', 'L1', 'C', 'R1', 'R2' ]
             knob_ndx: 3
             knob_title: 'Balance'
+            knob_turn_delay: .10
         MyFirstKnob:
-            knob_vals: ['L'+str(-x) for x in range(-50, 0)] + ['CENTER'] + ['R'+ str(x) for x in range(1, 51)]
+            knob_vals: [ 'L2', 'L1', 'C', 'R1', 'R2' ]
             knob_ndx: 1
-            knob_title: 'Pan'
+            knob_title: 'UnBalance'
+            knob_turn_delay: .1
         MyFirstKnob:
+            knob_turn_delay: .1
 
    
 '''
+active_knob = 0
 class MyFirstKnob(BoxLayout):
         
-    knob_title = StringProperty()
-    knob_vals  = ListProperty( [ str(i) for i in range(10) ] )
-    knob_ndx   = NumericProperty( 1 )
-    _mylast    = 0,0,0
+    knob_title      = StringProperty()
+    knob_vals       = ListProperty( [ str(i) for i in range(10) ] )
+    knob_ndx        = NumericProperty( 1 )
+    knob_turn_delay = 0.02
+    _mylast         = 0,0,0
 
+
+    def on_touch_up(self,touch):
+        global active_knob
+        active_knob = 0
+        return super().on_touch_up(touch)
+    def on_touch_down(self, touch):
+        global active_knob
+        for this in App.get_running_app().root.walk():
+            if (  'knob_title' in dir(this)
+                  and this.collide_point( touch.pos[0],touch.pos[1] )  
+               ):
+                this._mylast = touch.pos[0], touch.pos[1], time.time()
+                active_knob = this
+        return super().on_touch_down(touch)
 
     def on_touch_move(self, touch):
 
-        timedelta = 0.02  # time delta required to move a knob
-                          # might want to make this knob_turn_delay and let
-                          # it vary per instance.  Large val lists need short
-                          # times, but small val lists might do better with longer delay
 
         # Need better way to identify a knob than looking for knob_title in directory!
         # and i am probably looking at more widgets than I need to....
 
-        for this in App.get_running_app().root.walk():
-            if (  'knob_title' in dir(this)
-                  and this.collide_point( touch.pos[0],touch.pos[1] )  
-                  and ( time.time() - this._mylast[2] > timedelta ) ):
-
+        global active_knob
+        if active_knob and ( time.time() - active_knob._mylast[2] > active_knob.knob_turn_delay ) :
                  #   dy = touch.pos[1] - this._mylast[1] 
                  #   dx = touch.pos[0] - this._mylast[0] 
                  # if ( fabx(dy) > fabs(dx) ):
@@ -119,11 +130,11 @@ class MyFirstKnob(BoxLayout):
                  # else
                  #    want the sign of x
 
-                if   this._mylast[1] > touch.pos[1] or this._mylast[0] > touch.pos[0] :
-                   this.knob_ndx = max( this.knob_ndx - 1,  0 )
-                elif this._mylast[1] < touch.pos[1] or this._mylast[0] < touch.pos[0] :
-                   this.knob_ndx = min( this.knob_ndx + 1, len (this.knob_vals)-1 )
-                this._mylast = touch.pos[0], touch.pos[1], time.time()
+            if   active_knob._mylast[1] > touch.pos[1] or active_knob._mylast[0] > touch.pos[0] :
+               active_knob.knob_ndx = max( active_knob.knob_ndx - 1,  0 )
+            elif active_knob._mylast[1] < touch.pos[1] or active_knob._mylast[0] < touch.pos[0] :
+               active_knob.knob_ndx = min( active_knob.knob_ndx + 1, len (active_knob.knob_vals)-1 )
+            active_knob._mylast = touch.pos[0], touch.pos[1], time.time()
 
         return super().on_touch_move(touch)
 
