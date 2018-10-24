92f7780c2986df117711af9b7316543a3d1f1e75
sabrah12 <seth.abraham@gmail.com>
2018-10-23 22:03:28 -0700

    label/range settable. prettier colors and text. some bug fixes

--- xy.py
+++ xy.py
@@ -1,15 +1,16 @@
 
 import kivy
-from kivy.app import App
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.floatlayout import FloatLayout
+from kivy.app                import App
+from kivy.uix.boxlayout      import BoxLayout
+from kivy.uix.floatlayout    import FloatLayout
 from kivy.uix.relativelayout import RelativeLayout
-from kivy.lang import Builder
-from kivy.properties      import ListProperty
-from kivy.properties      import StringProperty
+from kivy.lang               import Builder
+from kivy.properties         import ListProperty
+from kivy.properties         import StringProperty
+from kivy.properties         import NumericProperty
 
-from math import atan2, degrees, fabs
-from random import random
+from math                    import atan2, degrees, fabs
+from random                  import random
 
 kivy.require('1.10.1')
 
@@ -28,20 +29,40 @@ kv = '''
 #:kivy 1.10.1
 #
 
+# from relative layout
 <XY_knob>
     size: mypad.size
     size_hint: (None,None)
     Label:
+        id: mypad
+        size: x_axis.width, x_axis.width
+        size_hint: (None,None)
+        canvas.after:
+            Line:
+                points: root.xy_knob_trackx
+            Line:
+                points: root.xy_knob_tracky
+            Line:
+                width: 4
+                points: root.xy_knob_val
+        canvas.before:
+            Color:
+                rgba: [ 0, 0, .8, 1 ]
+            Rectangle:
+                size: self.size
+    Label:
         id: x_axis
         size_hint_y: None
         height: 25
-        text: "I am the X axis"
+        text: root.xy_knob_xlab + " val= " + str( root.xy_knob_xmin )
+        font_size: 20
     Label:
         id: y_axis
         size_hint: (None,None)
         height: x_axis.height
         width: x_axis.width - x_axis.height
-        text: "I am the Y axis"
+        text: root.xy_knob_ylab + " val= " + str( root.xy_knob_ymin )
+        font_size: 20
         canvas.before:
             PushMatrix
             Rotate
@@ -53,27 +74,10 @@ kv = '''
             PopMatrix
     Label:
         id: trackinglabel
-        text: str( root.XY_knob_trkval )
+        text: str( root.xy_knob_trkval )
         text_size: self.size
         size: self.size
         size_hint: (None,None)
-    Label:
-        id: mypad
-        size: x_axis.width, x_axis.width
-        size_hint: (None,None)
-        canvas.after:
-            Line:
-                points: root.XY_knob_trackx
-            Line:
-                points: root.XY_knob_tracky
-            Line:
-                width: 4
-                points: root.XY_knob_val
-        canvas.before:
-            Color:
-                rgba: [ 0, 0, .8, .7 ]
-            Rectangle:
-                size: self.size
 #-----------------------
 BoxLayout:
     orientation: 'vertical'
@@ -95,11 +99,18 @@ BoxLayout:
         XY_knob:
             id: primo
             size: 250,250
-            size_hint:(None,None)
+            size_hint: (None,None)
+            xy_knob_xlab: 'Cut-off'
+            xy_knob_ylab: "Resonance"
         XY_knob:
             id: two
             size: 250,250
             size_hint:(None,None)
+            xy_knob_xlab: "Volume to 11"
+            xy_knob_ylab: "Balance(-1 to 1)"
+            xy_knob_xmax: 11
+            xy_knob_ymax: 1
+            xy_knob_ymin: -1
         XY_knob:
             id: three
             size: 250,250
@@ -113,28 +124,32 @@ BoxLayout:
 
 
 class XY_knob(RelativeLayout):
-#class XY_knob(FloatLayout):
-    XY_knob_trackx = ListProperty( [] )
-    XY_knob_tracky = ListProperty( [] )
-    XY_knob_trkval = StringProperty( '' )
-    XY_knob_val    = ListProperty([ [50, 50], [50,50] ] )
+    xy_knob_trackx = ListProperty( [] )
+    xy_knob_tracky = ListProperty( [] )
+    xy_knob_trkval = StringProperty( '' )
+    xy_knob_val    = ListProperty([ [0, 0], [0,0] ] )
+    xy_knob_xlab   = StringProperty( 'label x' )
+    xy_knob_xmin   = NumericProperty(   0 )
+    xy_knob_xmax   = NumericProperty( 100 )
+    xy_knob_ylab   = StringProperty( 'label y' )
+    xy_knob_ymin   = NumericProperty(   0 )
+    xy_knob_ymax   = NumericProperty( 100 )
   
 
     def on_touch_down(self,touch):
         if self.collide_point(*touch.pos):
             touch.grab(self)
-            # TODO: test that an explicit widget pos does not break things
             wigtch  = self.to_widget( *touch.pos )
             wigloc  = self.to_widget( *self.pos )
 
-            xval = int( wigtch[0] / self.width  * 100.0 )
-            yval = int( wigtch[1] / self.height * 100.0 )
-            self.XY_knob_trkval = '(' + str(xval) + ',' + str(yval)  +')'
+            xval = int( wigtch[0] / self.width  * (self.xy_knob_xmax - self.xy_knob_xmin)  ) + self.xy_knob_xmin
+            yval = int( wigtch[1] / self.height * (self.xy_knob_ymax - self.xy_knob_ymin)  ) + self.xy_knob_ymin
+            self.xy_knob_trkval = '(' + str(xval) + ',' + str(yval)  +')'
             self.ids.trackinglabel.pos = wigtch
 
 
-            self.XY_knob_tracky.extend( [[wigtch[0], wigloc[1]], [wigtch[0], wigloc[1] + self.height ] ] )
-            self.XY_knob_trackx.extend( [[wigloc[0] , wigtch[1]], [wigloc[0]+self.width, wigtch[1]] ] )
+            self.xy_knob_tracky.extend( [[wigtch[0], wigloc[1]], [wigtch[0], wigloc[1] + self.height ] ] )
+            self.xy_knob_trackx.extend( [[wigloc[0], wigtch[1]], [wigloc[0]+self.width, wigtch[1]] ] )
             return True
         return super().on_touch_down(touch)
 
@@ -150,19 +165,19 @@ class XY_knob(RelativeLayout):
                wigtch[i] = sorted([wigloc[i], wigloc[i]+ self.size[i], wigtch[i]])[1]
 
             # update and move the tracking label
-            xval = int( wigtch[0] / self.width  * 100.0 )
-            yval = int( wigtch[1] / self.height * 100.0 )
-            self.XY_knob_trkval = '(' + str(xval) + ',' + str(yval)  +')'
+            xval = int( wigtch[0] / self.width  * (self.xy_knob_xmax - self.xy_knob_xmin)  ) + self.xy_knob_xmin
+            yval = int( wigtch[1] / self.height * (self.xy_knob_ymax - self.xy_knob_ymin)  ) + self.xy_knob_ymin
+            self.xy_knob_trkval = '(' + str(xval) + ',' + str(yval)  +')'
             self.ids.trackinglabel.pos = wigtch
 
             # update tracking lines.  Do a copy so property notices update
-            self.XY_knob_tracky[0][0] = wigtch[0]
-            self.XY_knob_tracky[1][0] = wigtch[0]
-            self.XY_knob_tracky[:]    = self.XY_knob_tracky[:]
+            self.xy_knob_tracky[0][0] = wigtch[0]
+            self.xy_knob_tracky[1][0] = wigtch[0]
+            self.xy_knob_tracky[:]    = self.xy_knob_tracky[:]
 
-            self.XY_knob_trackx[0][1] = wigtch[1]
-            self.XY_knob_trackx[1][1] = wigtch[1]
-            self.XY_knob_trackx[:]    = self.XY_knob_trackx[:]
+            self.xy_knob_trackx[0][1] = wigtch[1]
+            self.xy_knob_trackx[1][1] = wigtch[1]
+            self.xy_knob_trackx[:]    = self.xy_knob_trackx[:]
 
             return True
         return super().on_touch_move(touch)
@@ -170,9 +185,9 @@ class XY_knob(RelativeLayout):
     def on_touch_up(self, touch):
         if touch.grab_current is self:
             touch.ungrab(self)
-            del(self.XY_knob_trackx[0:] )
-            del(self.XY_knob_tracky[0:] )
-            self.XY_knob_trkval = ''
+            del(self.xy_knob_trackx[0:] )
+            del(self.xy_knob_tracky[0:] )
+            self.xy_knob_trkval = ''
 
             #cleanup note:
             #common code with move should be exploited (code written once)
@@ -185,16 +200,16 @@ class XY_knob(RelativeLayout):
             for i in (0,1):
                wigtch[i] = sorted([wigloc[i], wigloc[i]+ self.size[i], wigtch[i]])[1]
 
-            xval = int( wigtch[0] / self.width  * 100.0 )
-            yval = int( wigtch[1] / self.height * 100.0 )
+            xval = int( wigtch[0] / self.width  * (self.xy_knob_xmax - self.xy_knob_xmin)  ) + self.xy_knob_xmin
+            yval = int( wigtch[1] / self.height * (self.xy_knob_ymax - self.xy_knob_ymin)  ) + self.xy_knob_ymin
           
             # re-title the axis with the new value
-            self.ids.x_axis.text = 'label type, val= ' + str(xval)
-            self.ids.y_axis.text = 'label type, val= ' + str(yval)
+            self.ids.x_axis.text = self.xy_knob_xlab + " val= " + str(xval)
+            self.ids.y_axis.text = self.xy_knob_ylab + " val= " + str(yval)
 
             # Move the dot to the new position
-            del(self.XY_knob_val[0:])
-            self.XY_knob_val.extend( ( wigtch, wigtch ) )
+            del(self.xy_knob_val[0:])
+            self.xy_knob_val.extend( ( wigtch, wigtch ) )
 
             return True
         return super().on_touch_up(touch)
@@ -204,7 +219,6 @@ class XY_knob(RelativeLayout):
 
 class XY_knobApp(App):
     def build(self):
-
         return Builder.load_string(kv)
 
 
