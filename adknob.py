5fcfae4dab20d3730fbb8b8b15675126ccfe5981
Elliot Garbus <elliotg2@cox.net>
2018-10-29 17:20:53 -0700

    Merge branch 'master' of https://github.com/sabrah12/sand_box into touch_update_00
    # Conflicts:
    #	xyknob.py

--- adknob.py
+++ adknob.py
@@ -2,22 +2,37 @@ from kivy.app import App
 from kivy.lang import Builder
 
 kv = '''
-BoxLayout:
-    orientation: 'vertical'
+FloatLayout:
     BoxLayout:
+        orientation: 'vertical'
+        BoxLayout:
+            Button:
+                text:'PL'        
+            
+            Label:
+                id:sq_pad
+                text: 'Square Pad'
+                size_hint_x: None
+                width: self.size[1]
+                canvas:
+                    Color:
+                        rgba: [1,0,0,.5]
+                    Line:
+                        width:5
+                        points:[sq_pad.pos, (sq_pad.pos[0] + sq_pad.width, sq_pad.pos[1] + sq_pad.height) ]
+            Button:
+                id:pad_right
+                text: 'PR'
         Button:
-            text:'PL'        
-        Button:
-            text: 'Square Pad'
-            size_hint_x: None
-            width: self.size[1]
-        Button:
-            text: 'PR'
-    Button:
-        text: 'Label goes here'
-        size_hint_y: None
-        height: self.texture_size[1]
-
+            text: 'Label goes here'
+            size_hint_y: None
+            height: self.texture_size[1]
+    Label:
+        text: 'Label from FloatLayout'
+        size_hint: (None, None)
+        color:[1,1,1,1]
+        size: self.texture_size
+        pos: (sq_pad.pos) 
 '''
 
 
