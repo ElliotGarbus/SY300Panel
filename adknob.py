ca9a765476c1914f2005fdc8df581741425ef51c
Elliot Garbus <elliotg2@cox.net>
2018-10-31 11:18:22 -0700

    updates

--- adknob.py
+++ adknob.py
@@ -1,61 +1,64 @@
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.floatlayout import FloatLayout
-from kivy.uix.relativelayout import RelativeLayout
+from kivy.properties import NumericProperty
+
 
 kv = '''
 <ADKnob>
-    #BoxLayout:
     orientation: 'vertical'
     BoxLayout:
         Label:
             id:pad_left
             text:'L'
             size_hint_x:.001
-        #Label:
-        #    id: x_axis_label
-        #    text:'X-Axis Label'
-        #    size_hint_x: None
-        #    width: self.texture_size[1]
-        #    canvas.before:
-        #        PushMatrix
-        #        Rotate
-        #            angle: 90
-        #            origin: self.center
-        #    canvas.after:
-        #        PopMatrix         
         Label:
             id:sq_pad
-            text: 'Square Pad'
+            text: '-50'
+            font_size: 15
             size_hint_x: None
             width: self.size[1]
-            canvas:
+            canvas.before:
                 Color:
-                    rgba: [1,0,0,.5]
-                Line:
-                    width:5
-                    points:[sq_pad.pos, (sq_pad.pos[0] + sq_pad.width, sq_pad.pos[1] + sq_pad.height)]
+                    rgba: [.4, .4 , .4, .5 ]
+                Line:  # Middle Bar
+                    width:4
+                    cap: 'none'
+                    points:[(sq_pad.center_x,sq_pad.pos[1]), (sq_pad.center_x, sq_pad.top)]
+                Color:
+                    rgba: [0, 0 , 1, .9]
+                Line: # Attack Line
+                    width: 2
+                    cap: 'none'
+                    points: [sq_pad.center_x - 6,sq_pad.pos[1], sq_pad.center_x - 6, sq_pad.top] #offset from center
+                Line: # Decay Line
+                    width: 2
+                    cap: 'none'   
+                    points: [sq_pad.center_x + 6 ,sq_pad.pos[1], sq_pad.center_x + 6, sq_pad.top] #offset from center
+            
                 Color:
                     rgba:[1,1,1,1]
                 Line:
                     width:2
                     rectangle: (*self.pos,self.width,self.height)
+
+                
         Label:
             id:pad_right
             text: 'R'
             size_hint_x: .001 
-    Label:
-        text: 'Y-Axis Label'
-        size_hint_y: None
-        height: self.texture_size[1]
+    #Label:
+    #    text: 'Y-Axis Label'
+    #    size_hint_y: None
+    #    height: self.texture_size[1]
     Label:
         text: 'AMP ENV'
+        font_size: 15
         size_hint_y: None
         height: self.texture_size[1]
     
     Label:
-        text: 'Test pos'
+        text: 'Test pos:' # + str()
         size_hint: (None, None)
         color:[1,1,1,1]
         size: self.texture_size
@@ -64,13 +67,18 @@ kv = '''
 
 
 class ADKnob(BoxLayout):
+    adknob_decay_value = NumericProperty(0)
+    adknob_attack_value = NumericProperty(0)
+    adknob_ndx = NumericProperty(0)
 
     def on_touch_down(self, touch):
         if self.ids.sq_pad.collide_point(*touch.pos):
-            sq_xy = [touch.pos[i]-self.ids.sq_pad.pos[i] for i in range(2)]
-            scale_xy = [sq_xy[0] * 100//self.ids.sq_pad.width, sq_xy[1] * 100//self.ids.sq_pad.height]
+            sq_xy = self.ids.sq_pad.to_widget(*touch.pos, True)
+            scale_xy = [int(sq_xy[0] * 100/(self.ids.sq_pad.width - 1)), int(sq_xy[1] * 100/(self.ids.sq_pad.height - 1))]
             print('Self transformed pos:', sq_xy)
             print('Transformed and scaled', scale_xy)
+            print('pos', touch.pos)
+            print('to local', self.ids.sq_pad.to_widget(*touch.pos, True))
 
         return super().on_touch_down(touch)
 
